5cbc1cd98fe0d5f298ce1fb32279fc8c1841538b
nlaz <nikolazaris@utexas.edu>
2018-05-16 19:42:37 -0400

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 22000, // was 8545
+      port: 22000,
       network_id: "*", // Match any network id
       gasPrice: 0,
       gas: 4500000,
